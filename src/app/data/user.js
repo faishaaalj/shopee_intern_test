447428b2a8beebb5acd8269d4b679f143d01ddfb
faishallj <fjanuarahman@gmail.com>
2024-08-02 17:44:06 +0700

    finalize

---
+++ src/app/data/user.js
@@ -0,0 +1,131 @@
+const userDummy = [
+    {
+        key: '1',
+        firstName: 'Faishal',
+        lastName: 'Januarahman',
+        age: '23',
+        address: 'Tasikmalaya, Jawa Barat'
+    },
+    {
+        key: '2',
+        firstName: 'Rizal',
+        lastName: 'Fauzi',
+        age: '22',
+        address: 'Bandung, Jawa Barat'
+    },
+    {
+        key: '3',
+        firstName: 'Putri',
+        lastName: 'Tanjung',
+        age: '28',
+        address: 'DKI Jakarta'
+    },
+    {
+        key: '4',
+        firstName: 'Nuzulfio',
+        lastName: 'Barizcky',
+        age: '25',
+        address: 'Kediri, Jawa Timur'
+    },
+    {
+        key: '5',
+        firstName: 'Rifki',
+        lastName: 'Adi',
+        age: '27',
+        address: 'Kebumen, Jawa Tengah'
+    },
+    {
+        key: '6',
+        firstName: 'Nadja',
+        lastName: 'Shafira',
+        age: '20',
+        address: 'DKI Jakarta'
+    },
+    {
+        key: '7',
+        firstName: 'Nur',
+        lastName: 'Ilyas',
+        age: '26',
+        address: 'Surabaya, Jawa Timur'
+    },
+    {
+        key: '8',
+        firstName: 'Zendy',
+        lastName: 'Bramantia',
+        age: '23',
+        address: 'Banda Aceh, Aceh'
+    },
+    {
+        key: '9',
+        firstName: 'Sanjaya',
+        lastName: 'Suryadinata',
+        age: '31',
+        address: 'Ciamis, Jawa Barat'
+    },
+    {
+        key: '10',
+        firstName: 'Syauqi',
+        lastName: 'Hawari',
+        age: '25',
+        address: 'Garut, Jawa Barat'
+    },
+    {
+        key: '11',
+        firstName: 'Nur',
+        lastName: 'Ilyas',
+        age: '26',
+        address: 'Surabaya, Jawa Timur'
+    },
+    {
+        key: '12',
+        firstName: 'Zendy',
+        lastName: 'Bramantia',
+        age: '13',
+        address: 'Banda Aceh, Aceh'
+    },
+    {
+        key: '13',
+        firstName: 'Sanjaya',
+        lastName: 'Suryadinata',
+        age: '31',
+        address: 'Ciamis, Jawa Barat'
+    },
+    {
+        key: '14',
+        firstName: 'Syauqi',
+        lastName: 'Hawari',
+        age: '25',
+        address: 'Garut, Jawa Barat'
+    },
+    {
+        key: '15',
+        firstName: 'Faishal',
+        lastName: 'Januarahman',
+        age: '23',
+        address: 'Tasikmalaya, Jawa Barat'
+    },
+    {
+        key: '16',
+        firstName: 'Rizal',
+        lastName: 'Fauzi',
+        age: '22',
+        address: 'Bandung, Jawa Barat'
+    },
+    {
+        key: '17',
+        firstName: 'Putri',
+        lastName: 'Tanjung',
+        age: '28',
+        address: 'DKI Jakarta'
+    },
+    {
+        key: '18',
+        firstName: 'Nuzulfio',
+        lastName: 'Barizcky',
+        age: '25',
+        address: 'Kediri, Jawa Timur'
+    },
+    
+  ];
+
+export default userDummy;
